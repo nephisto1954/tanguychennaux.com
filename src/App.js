baa1412a79665d7acb35341c6a1d165d53a3e32b
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-04-22 17:12:09 +0100

    Testing script src issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
         <h1>Tanguy Chennaux website coming soon!!!</h1>
-        <p>Almost there!</p>
+        <p>Almost there!!!</p>
     </div>
   );
 }
