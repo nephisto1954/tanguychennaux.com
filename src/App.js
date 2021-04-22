d72987208bfcd41fac57a4d1e354f81d0f8d88e3
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-04-22 14:36:04 +0100

    Testing lambdaEdge setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://www.tanguychennaux.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tanguy Chennaux Website coming soon!!!
-        </a>
-      </header>
+        <h1>Tanguy Chennaux website coming soon</h1>
     </div>
   );
 }
