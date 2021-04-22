52c2a890bc2489384d234d065853e212609eeff8
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-04-22 14:39:13 +0100

    Testing lambdaEdge setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Tanguy Chennaux website coming soon</h1>
+        <h1>Tanguy Chennaux website coming soon!!!</h1>
     </div>
   );
 }
