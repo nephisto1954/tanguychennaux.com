6bc5fa4279a8ad7e662a18ff464e94d8ab9bde7c
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-04-22 11:13:43 +0100

    Testing Travis CI deployment, changed test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tanguy Chennaux Website coming soon!!!/i);
   expect(linkElement).toBeInTheDocument();
 });
