8c9b8fa27f5e8edd9c2fc94c4871ff040744c0d3
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 16:49:46 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { JSDOM } from 'jsdom';
+const { window } = new JSDOM(`...`);
+window.URL.createObjectURL = function() {};
+
 test('renders the Canvas', () => {
   render(<App />);
   const rootElem = screen.getByTestId(/root/i);
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-window.URL.createObjectURL = function() {};
 
 import { JSDOM } from 'jsdom';
-
 const { window } = new JSDOM(`...`);
+window.URL.createObjectURL = function() {};
+
+import '@testing-library/jest-dom';
