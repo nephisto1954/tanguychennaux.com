8b72846f7d0287316bfdd0cd8586094507a7bb97
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 15:41:24 +0200

    testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// window.URL.createObjectURL = function() {};
+window.URL.createObjectURL = function() {};
 import '@testing-library/jest-dom';
