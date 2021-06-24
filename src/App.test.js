6b1cc3cb1232ffb5ccda718d06c80af184830166
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 15:26:37 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,8 @@
-import React from 'react'
-import ReactThreeTestRenderer from '@react-three/test-renderer'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-const Mesh = () => {
-  const meshRef = React.useRef()
-  useFrame((_, delta) => {
-    meshRef.current.rotation.x += delta
-  })
-
-  return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[2, 2]} />
-      <meshBasicMaterial />
-    </mesh>
-  )
-}
-
-const renderer = ReactThreeTestRenderer.create(<Mesh />)
-
-// // expect(renderer.scene.children[0].instance.rotation.x).toEqual(0)
-
-// ReactThreeTestRenderer.act(() => {
-//   renderer.advanceFrames(2, 1)
-// })
-
-// // expect(renderer.scene.children[0].instance.rotation.x).toEqual(2)
+test('renders the Canvas', () => {
+  render(<App />);
+  const rootElem = screen.getByTestId(/root/i);
+  expect(rootElem).toBeInTheDocument();
+});
