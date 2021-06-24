eacf701b2371b10dc5a602c6527b3f5d13eb80a1
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 12:31:03 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import ReactThreeTestRenderer from '@react-three/test-renderer'
+import ReactThreeTestRenderer from 'react-three-test-renderer'
 
-const renderer = ReactThreeTestRenderer.create(
-  <mesh>
-    <boxBufferGeometry args={[2, 2]} />
-    <meshStandardMaterial
-      args={[
-        {
-          color: 0x0000ff,
-        },
-      ]}
-    />
-  </mesh>,
-)
+const Mesh = () => {
+  const meshRef = React.useRef()
+  useFrame((_, delta) => {
+    meshRef.current.rotation.x += delta
+  })
 
-// // assertions using the TestInstance & Scene Graph
-// console.log(renderer.toGraph())
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2, 2]} />
+      <meshBasicMaterial />
+    </mesh>
+  )
+}
+
+const renderer = await ReactThreeTestRenderer.create(<Mesh />)
+
+expect(renderer.scene.children[0].instance.rotation.x).toEqual(0)
+
+await ReactThreeTestRenderer.act(async () => {
+  await renderer.advanceFrames(2, 1)
+})
+
+expect(renderer.scene.children[0].instance.rotation.x).toEqual(2)
