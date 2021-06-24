18b71793c9997e8dd3c0cf0614910c3dcd857fb9
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 16:59:28 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-import { JSDOM } from 'jsdom';
-const { window } = new JSDOM(`...`);
-window.URL.createObjectURL = function() {};
-
 test('renders the Canvas', () => {
   render(<App />);
   const rootElem = screen.getByTestId(/root/i);
