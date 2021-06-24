bf187ebe979076881952230cf4f55e535055bf48
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 16:44:45 +0200

    testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+window.URL.createObjectURL = function() {};
 
 import { JSDOM } from 'jsdom';
 
 const { window } = new JSDOM(`...`);
-
-window.URL.createObjectURL = function() {};
