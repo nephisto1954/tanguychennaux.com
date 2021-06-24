5f53406995f7362a35748673a008ed721f6fa18d
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 16:10:01 +0200

    testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-window.URL.createObjectURL = function() {};
 import '@testing-library/jest-dom';
+
+import { JSDOM } from 'jsdom';
+
+let windowSpy: any;
+beforeEach(() => {
+    windowSpy = jest.spyOn(global as any, 'window', 'get');
+});
+afterEach(() => {
+    windowSpy.mockRestore();
+});
+
+describe('', () => {
+    it ('', () => {
+        const { window } = new JSDOM();
+        windowSpy.mockImplementation(() => window);
+        // now you have `window` in test environment
+    });
+});
+window.URL.createObjectURL = function() {};
