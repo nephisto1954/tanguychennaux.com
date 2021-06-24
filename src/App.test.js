f69bb4410d713abfd2eba00b41e0809ec54d3d8a
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-24 10:40:26 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactThreeTestRenderer from '@react-three/test-renderer'
 
-test('renders the Canvas', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId(/TanguyChennaux/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const renderer = await ReactThreeTestRenderer.create(
+  <mesh>
+    <boxBufferGeometry args={[2, 2]} />
+    <meshStandardMaterial
+      args={[
+        {
+          color: 0x0000ff,
+        },
+      ]}
+    />
+  </mesh>,
+)
+
+// assertions using the TestInstance & Scene Graph
+console.log(renderer.toGraph())
