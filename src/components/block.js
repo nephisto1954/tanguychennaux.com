efed449971f2f68f09c6097941f0d1ad71f66036
tanguy chennaux <tanguy.chennaux@gmail.com>
2021-06-28 10:47:39 +0200

    cleanup code

--- src/components/block.js
+++ src/components/block.js
@@ -1,27 +1,8 @@
-import * as THREE from "three"
-import React, { createContext, useRef, useContext } from "react"
-import { useFrame, useThree } from "@react-three/fiber"
+import { createContext, useContext } from "react"
+import { useThree } from "@react-three/fiber"
 import state from "../store"
 
 const offsetContext = createContext(0)
-
-function Block({ children, offset, factor, ...props }) {
-  const { offset: parentOffset, sectionHeight } = useBlock()
-  const ref = useRef()
-  offset = offset !== undefined ? offset : parentOffset
-  useFrame(() => {
-    const curY = ref.current.position.y
-    const curTop = state.top.current
-    ref.current.position.y = THREE.MathUtils.lerp(curY, (curTop / state.zoom) * factor, 0.1)
-  })
-  return (
-    <offsetContext.Provider value={offset}>
-      <group {...props} position={[0, 0, 0]}>
-        <group ref={ref}>{children}</group>
-      </group>
-    </offsetContext.Provider>
-  )
-}
 
 function useBlock() {
   const { sections, pages } = state
@@ -49,4 +30,4 @@ function useBlock() {
   }
 }
 
-export { Block, useBlock }
+export { useBlock }
